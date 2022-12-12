bdefe3f1f19528f1c545868ab6b09d74d657c546
Wang-Zhenchuan <99259301+Wang-Zhenchuan@users.noreply.github.com>
2022-12-12 23:34:50 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -477,7 +477,7 @@ S.Dot.prototype = {
 
 
 S.ShapeBuilder = (function () {
-  var gap = 13,
+  var gap = 8,
       shapeCanvas = document.createElement('canvas'),
       shapeContext = shapeCanvas.getContext('2d'),
       fontSize = 500,
